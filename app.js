6221c1a42b24bc8038f25baaaaa0dd74b0122e0b
kouevi ekoue <kouevi.ekoue@student.vinci.be>
2024-11-13 17:28:53 +0100

    Fonftion des block de 5 heures

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ export class berlinClock {
 
     getSeconds(seconds) {
         if (seconds % 2 === 0) {
-            return 'Allumée';
+            return 'R'; // R -> Pour Red quand c'est allumée
         }else{
-            return 'Eteint';
+            return 'O'; // O -> Pour Off quand c'est éteint
         }
+    }
+
+    getFiveHours(hours) {
+        let row = '';
+        let litBlock = Math.floor(hours / 5);
+
+        for (let i = 0; i < 4; i++) {
+            if (i < litBlock){
+                row += 'R'; // R -> Pour Red quand c'est allumée
+            } else{
+                row += 'O'; // O -> Pour Off quand c'est éteint
+            }
+        }
+        return row;
     }
 }
